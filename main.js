e8216f9c3ecf8cc0a40d19a8757babcc330cea96
Zaigham Haider <152426788+zaighamhaider7@users.noreply.github.com>
2024-06-24 12:05:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function darkmode(){
+    document.getElementById("body").style.backgroundColor="black"
+    document.getElementById("body").style.color="white"
+    document.getElementById("modes").style.backgroundColor="black"
+    document.getElementById("modes").style.transition="0.8s"
+    document.getElementById("login").style.backgroundColor="green"
+    document.getElementById("free").style.backgroundColor="green"
+    document.getElementById("secndfreetrail").style.backgroundColor="green"
+}
+function lightmode(){
+    document.getElementById("body").style.backgroundColor="skyblue"
+    document.getElementById("body").style.color="black"
+    document.getElementById("modes").style.backgroundColor="white"
+    document.getElementById("modes").style.transition="0.8s"
+    document.getElementById("login").style.backgroundColor="black"
+    document.getElementById("free").style.backgroundColor="black"
+    document.getElementById("secndfreetrail").style.backgroundColor="black"
+}
